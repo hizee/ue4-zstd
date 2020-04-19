d0b8cb313056ebbbbdcc1d6255152693affea5c8
hxhb <hxhb@live.com>
2020-04-19 19:52:36 +0800

    implementated zstd in unlua

--- Source/libzstd/Private/libzstd.cpp
+++ Source/libzstd/Private/libzstd.cpp
@@ -2,7 +2,10 @@
 
 #include "libzstd.h"
 
+#include "zstd.h"
+
 #define LOCTEXT_NAMESPACE "FlibzstdModule"
+#define COMPRESS_LEVEL 19
 
 FName FZstdCompressionFormat::GetCompressionFormatName()
 {
@@ -10,18 +13,41 @@ FName FZstdCompressionFormat::GetCompressionFormatName()
 }
 bool FZstdCompressionFormat::Compress(void* CompressedBuffer, int32& CompressedSize, const void* UncompressedBuffer, int32 UncompressedSize, int32 CompressionData)
 {
-	UE_LOG(LogTemp, Log, TEXT("FZstdCompressionFormat::Compress"));
+	//UE_LOG(LogTemp, Log, TEXT("FZstdCompressionFormat::Compress"));
+	//UE_LOG(LogTemp, Log, TEXT("CompressedBuffer:%x,CompressedSize:%d,UnCompressedBuffer:%x,UnCompressedSize:%d,CompressionData:%d"),CompressedBuffer,CompressedSize,UncompressedBuffer,UncompressedSize,CompressionData);
+
+	int32 Result = ZSTD_compress(CompressedBuffer, CompressedSize, UncompressedBuffer, UncompressedSize, COMPRESS_LEVEL);
+	if (Result > 0)
+	{
+		if (Result > GetCompressedBufferSize(UncompressedSize, CompressionData))
+		{
+			FPlatformMisc::LowLevelOutputDebugStringf(TEXT("%d < %d"), Result, GetCompressedBufferSize(UncompressedSize, CompressionData));
+			// we cannot safely go over the BufferSize needed!
+			return false;
+		}
+		CompressedSize = Result;
+		return true;
+	}
 	return false;
 }
 bool FZstdCompressionFormat::Uncompress(void* UncompressedBuffer, int32& UncompressedSize, const void* CompressedBuffer, int32 CompressedSize, int32 CompressionData)
 {
-	UE_LOG(LogTemp, Log, TEXT("FZstdCompressionFormat::Uncompress"));
+	//UE_LOG(LogTemp, Log, TEXT("FZstdCompressionFormat::Uncompress"));
+	//UE_LOG(LogTemp, Log, TEXT("UncompressedBuffer:%x,UncompressedSize:%d,CompressedBuffer:%x,CompressedSize:%d,CompressionData:%d"), UncompressedBuffer, UncompressedSize, CompressedBuffer, CompressedSize, CompressionData);
+
+	int32 Result = ZSTD_decompress(UncompressedBuffer, UncompressedSize, CompressedBuffer, CompressedSize);
+	if (Result > 0)
+	{
+		UncompressedSize = Result;
+		return true;
+	}
 	return false;
 }
 int32 FZstdCompressionFormat::GetCompressedBufferSize(int32 UncompressedSize, int32 CompressionData)
 {
-	UE_LOG(LogTemp, Log, TEXT("FZstdCompressionFormat::GetCompressedBufferSize"));
-	return 0;
+	//UE_LOG(LogTemp, Log, TEXT("UncompressedSize:%d,CompressionData:%d"), UncompressedSize, CompressionData);
+	//UE_LOG(LogTemp, Log, TEXT("FZstdCompressionFormat::GetCompressedBufferSize"));
+	return ZSTD_compressBound(UncompressedSize);
 }
 
 void FlibzstdModule::StartupModule()
